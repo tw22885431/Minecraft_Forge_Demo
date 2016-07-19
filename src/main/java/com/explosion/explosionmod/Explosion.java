986361c45032604f18ffd0f11bc49f02e477c18f
Ben Wu <re22885431@gmail.com>
2016-07-19 19:49:47 +0800

    First Minecraft Forge Demo (incomplete)

---
+++ src/main/java/com/explosion/explosionmod/Explosion.java
@@ -0,0 +1,7 @@
+package com.explosion.explosionmod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+	public class 
